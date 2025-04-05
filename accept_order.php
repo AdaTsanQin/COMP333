ee5e94a4223a7bed584f53e6711567871835092f
Ada Qin <adaqin@Adas-MacBook-Air-5803.local>
2025-04-05 17:52:11 -0400

    Make sure new version (issue#3.4)

--- accept_order.php
+++ accept_order.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:8081");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, PUT");
 header("Access-Control-Allow-Headers: Content-Type");
