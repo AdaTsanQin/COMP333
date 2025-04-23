b2582f8168ab56270b9676caf9fb6e17e9f7e76d
2274006014 <hwang10@wesleyan.edu>
2025-04-23 15:59:49 -0400

    11

--- App.js
+++ App.js
@@ -1,46 +1,61 @@
+// App.js – merged (Chat + Map Navigate) version
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-/* --- 页面组件 --- */
-import HomeScreen               from './screen/HomeScreen';
-import LoginScreen              from './screen/LoginScreen';
-import RegisterScreen           from './screen/RegisterScreen';
-import DashboardScreen          from './screen/DashboardScreen';
-import CreateRequestScreen      from './screen/CreateRequestScreen';
-import ViewRequestScreen        from './screen/ViewRequestScreen';
-import AcceptOrderScreen        from './screen/AcceptOrderScreen';
-import SearchScreen             from './screen/SearchScreen';
-import CreateStoreRequestScreen from './screen/CreateStoreRequestScreen';
-import ChatScreen               from './screen/ChatScreen';
-import ChatListScreen           from './screen/ChatListScreen';   // new
+/* ---------- screens ---------- */
+import HomeScreen                 from './screen/HomeScreen';
+import LoginScreen                from './screen/LoginScreen';
+import RegisterScreen             from './screen/RegisterScreen';
+import DashboardScreen            from './screen/DashboardScreen';
 
+import CreateRequestScreen        from './screen/CreateRequestScreen';
+import CreateStoreRequestScreen   from './screen/CreateStoreRequestScreen';
+import ViewRequestScreen          from './screen/ViewRequestScreen';
+import AcceptOrderScreen          from './screen/AcceptOrderScreen';
+
+import SearchScreen               from './screen/SearchScreen';
+
+import ChatListScreen             from './screen/ChatListScreen';
+import ChatScreen                 from './screen/ChatScreen';
+
+import NavigationToLocationScreen from './screen/NavigationToLocationScreen';   // ★ map-navigate
+
+/* ---------- navigator ---------- */
 const Stack = createStackNavigator();
 
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
-      <React.Fragment>
-        <Stack.Screen name="Home"                   component={HomeScreen} />
-        <Stack.Screen name="Login"                  component={LoginScreen} />
-        <Stack.Screen name="Register"               component={RegisterScreen} />
-        <Stack.Screen name="Dashboard"              component={DashboardScreen} />
+      {/* auth / entry */}
+      <Stack.Screen name="Home"     component={HomeScreen} />
+      <Stack.Screen name="Login"    component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
 
-        {/* 用户下单 */}
-        <Stack.Screen name="CreateRequestScreen"      component={CreateRequestScreen} />
-        <Stack.Screen name="CreateStoreRequestScreen" component={CreateStoreRequestScreen} />
+      {/* dashboard */}
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
 
-        {/* 列表 / 接单 */}
-        <Stack.Screen name="ViewRequestScreen"      component={ViewRequestScreen} />
-        <Stack.Screen name="AcceptOrderScreen"      component={AcceptOrderScreen} />
+      {/* user flow */}
+      <Stack.Screen name="CreateRequestScreen"        component={CreateRequestScreen} />
+      <Stack.Screen name="CreateStoreRequestScreen"   component={CreateStoreRequestScreen} />
+      <Stack.Screen name="ViewRequestScreen"          component={ViewRequestScreen} />
 
-        {/* 商品搜索 */}
-        <Stack.Screen name="SearchScreen"           component={SearchScreen} />
+      {/* dasher flow */}
+      <Stack.Screen name="AcceptOrderScreen" component={AcceptOrderScreen} />
 
-        {/* 聊天相关 */}
-        <Stack.Screen name="Chats"                  component={ChatListScreen} />
-        <Stack.Screen name="Chat"                   component={ChatScreen} />
-      </React.Fragment>
+      {/* product search */}
+      <Stack.Screen name="SearchScreen" component={SearchScreen} />
+
+      {/* chat */}
+      <Stack.Screen name="Chats" component={ChatListScreen} />
+      <Stack.Screen name="Chat"  component={ChatScreen} />
+
+      {/* map navigation */}
+      <Stack.Screen
+        name="NavigationToLocationScreen"
+        component={NavigationToLocationScreen}
+        options={{ title: 'Navigation' }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
